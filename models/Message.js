d86b5ea4fe012e84d3bee6ec0ef0821d78efbbca
sumitsharma17361-ui <sumitsharma17361@gmail.com>
2026-07-20 18:13:51 +0530

    Update Message.js

--- models/Message.js
+++ models/Message.js
@@ -1,8 +1,13 @@
 const mongoose = require('mongoose');
+
 const MessageSchema = new mongoose.Schema({
   sender: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
   receiver: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-  text: { type: String, required: true },
+  text: { type: String }, // Text optional ho gaya kyuki sirf file bhi bhej sakte hain
+  fileUrl: { type: String, default: null },   // File ka download link
+  fileName: { type: String, default: null },  // File ka asli naam
+  fileType: { type: String, default: null },  // image, video, audio, document etc.
   timestamp: { type: Date, default: Date.now }
 });
+
 module.exports = mongoose.model('Message', MessageSchema);
