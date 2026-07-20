156b8e656d0cb529175d3bc69558e5fa4305cd64
sumitsharma17361-ui <sumitsharma17361@gmail.com>
2026-07-20 22:25:13 +0530

    Update User.js

--- models/User.js
+++ models/User.js
@@ -2,11 +2,12 @@ const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
+  phoneNumber: { type: String, unique: true, sparse: true }, // Mobile number login support
   password: { type: String, required: true },
   friends: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
   friendRequests: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
   isOnline: { type: Boolean, default: false },
-  profilePic: { type: String, default: "" }, // Base64 dynamic profile avatar storage
+  profilePic: { type: String, default: "" },
   lastSeen: { type: Date, default: Date.now }
 });
 
