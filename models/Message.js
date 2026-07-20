52c96e724d95a4c2b4bfc2735bb839f6e925b547
sumitsharma17361-ui <sumitsharma17361@gmail.com>
2026-07-20 19:20:38 +0530

    Update Message.js

--- models/Message.js
+++ models/Message.js
@@ -3,10 +3,11 @@ const mongoose = require('mongoose');
 const MessageSchema = new mongoose.Schema({
   sender: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
   receiver: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-  text: { type: String }, // Text optional ho gaya kyuki sirf file bhi bhej sakte hain
-  fileUrl: { type: String, default: null },   // File ka download link
-  fileName: { type: String, default: null },  // File ka asli naam
-  fileType: { type: String, default: null },  // image, video, audio, document etc.
+  text: { type: String },
+  fileUrl: { type: String, default: null },
+  fileName: { type: String, default: null },
+  fileType: { type: String, default: null },
+  status: { type: String, enum: ['sent', 'delivered', 'read'], default: 'sent' }, // WhatsApp Ticks Tracking
   timestamp: { type: Date, default: Date.now }
 });
 
