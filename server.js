6bf2fe4af1c94ab63ebccb0e71418045c0f5d71e
sumitsharma17361-ui <sumitsharma17361@gmail.com>
2026-07-25 11:31:06 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-Require('dotenv').config();
+require('dotenv').config();
 const express = require('express');
 const http = require('http');
 const mongoose = require('mongoose');
@@ -298,4 +298,3 @@ io.on('connection', (socket) => {
 
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-        
